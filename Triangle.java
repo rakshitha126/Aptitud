d4023f15886bd58e51b712a506f2a4e655dda7de
rakshitha126 <nrakshitha661@gmail.com>
2021-07-24 19:10:53 +0530

    program based on Aptitud

---
+++ Triangle.java
@@ -0,0 +1,26 @@
+class Triangle
+{
+	public static void main(String a[])
+	{
+		Triangle triangle=new Triangle();
+		System.out.println("Triangle object is created");
+		triangle.Triangle();
+		
+	}
+	
+	public void Triangle()
+	{
+		for(int r=1;r<=5;r++)
+		{
+			for(int c=1;c<=5;c++)
+			{
+
+				System.out.print("*");
+			}
+			
+		
+	
+	System.out.println();
+}
+	}
+}
